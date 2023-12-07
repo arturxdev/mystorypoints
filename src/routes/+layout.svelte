e79ebbd6f96372cdd88279e3af42bf40d5b18dd6
Arturo GUerrero <arturo.guerrero@koibanx.com>
2023-12-06 20:31:53 -0600

    feat: add link to twitter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,4 @@
 
 <slot />
 
-<p class="text-center my-5">Hecho con ❤️ por @arturoxdev</p>
+<p class="text-center my-5">Hecho con ❤️ por <a class="text-indigo-500" target="_blank_" href="https://twitter.com/arturoxdev">@arturoxdev</a> </p>
